0e1fc2c1b35a8c6fbf6d3151ee10dd3154abda10
bruingineer <joshua.g.camarena@gmail.com>
2021-09-28 21:13:51 -0700

    shooter sub

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -42,12 +42,12 @@ public class Robot extends TimedRobot {
     System.out.println("RobotInit-----------");
     setNetworkTablesFlushEnabled(true);
     m_trajectory = TrajectoryGenerator.generateTrajectory(
-        new Pose2d(2, 2, new Rotation2d(0)),
+        new Pose2d(0, 0, new Rotation2d(0)),
         List.of(
-          new Translation2d(8, 2),
-          new Translation2d(8, 5)
+          new Translation2d(1.5, 0),
+          new Translation2d(1.5, 3)
           ), 
-        new Pose2d(2, 5, new Rotation2d(Math.PI)), 
+        new Pose2d(0, 3, new Rotation2d(Math.PI)), 
         new TrajectoryConfig(1, 1)
       );
   }
---
+++ src/main/java/frc/robot/shooter.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.SlewRateLimiter;
+
+public class shooter {
+  /** Creates a new shooter. */
+  private final XboxController m_controller = new XboxController(0);
+  private final SlewRateLimiter m_speedLimiter = new SlewRateLimiter(4);
+  //  motor controllers
+  private final WPI_TalonFX m_leftShooter = new WPI_TalonFX(21);
+  private final WPI_TalonFX m_rightShooter = new WPI_TalonFX(22);
+  
+
+  public shooter() {
+    
+  }
+
+  public void robotPeriodic() {
+
+  }
+
+  public void autonomousInit() {
+
+  }
+
+  public void autonomousPeriodic() {
+
+  }
+
+  public void teleopInit() {
+
+  }
+
+  public void teleopPeriodic() {
+    boolean isPressed = m_controller.getAButton();
+    double speed = 0;
+    if (isPressed) {
+      speed = 0.3;
+    }
+    else {
+      speed = 0;
+    }
+    m_leftShooter.set(ControlMode.PercentOutput, m_speedLimiter.calculate(speed));
+  }
+
+  public void disabledInit() {
+
+  }
+}
